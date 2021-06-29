180b0031c1eb92a121843cb9c60df72106f68881
thetechguy-cpu <57086616+thetechguy-cpu@users.noreply.github.com>
2021-06-29 13:16:31 +0800

    Update config.js
    Removed slash commands support and icon.

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ module.exports = {
   Token: process.env.Token || "", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
-  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  Scopes: ["identify", "guilds"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Pikachu is cute", //A Secret like a password
   IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
   Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
